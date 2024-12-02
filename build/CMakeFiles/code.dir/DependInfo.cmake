828256c9e26fc02b65161f10c698232931259e42
zxsheather <zxsheather@sjtu.edu.cn>
2024-12-02 15:38:15 +0800

    update

---
+++ build/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/Basic.cpp" "CMakeFiles/code.dir/Basic/Basic.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/Basic.cpp.o.d"
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/Utils/error.cpp" "CMakeFiles/code.dir/Basic/Utils/error.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/Utils/error.cpp.o.d"
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/Utils/strlib.cpp" "CMakeFiles/code.dir/Basic/Utils/strlib.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/Utils/strlib.cpp.o.d"
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/Utils/tokenScanner.cpp" "CMakeFiles/code.dir/Basic/Utils/tokenScanner.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/Utils/tokenScanner.cpp.o.d"
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/evalstate.cpp" "CMakeFiles/code.dir/Basic/evalstate.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/evalstate.cpp.o.d"
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/exp.cpp" "CMakeFiles/code.dir/Basic/exp.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/exp.cpp.o.d"
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/parser.cpp" "CMakeFiles/code.dir/Basic/parser.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/parser.cpp.o.d"
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/program.cpp" "CMakeFiles/code.dir/Basic/program.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/program.cpp.o.d"
+  "/Users/apple/Desktop/Programming/ACMOJ/Basic-Interpreter-2024/Basic/statement.cpp" "CMakeFiles/code.dir/Basic/statement.cpp.o" "gcc" "CMakeFiles/code.dir/Basic/statement.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/Basic/Basic.cpp.o"
+  "CMakeFiles/code.dir/Basic/Basic.cpp.o.d"
+  "CMakeFiles/code.dir/Basic/Utils/error.cpp.o"
+  "CMakeFiles/code.dir/Basic/Utils/error.cpp.o.d"
+  "CMakeFiles/code.dir/Basic/Utils/strlib.cpp.o"
+  "CMakeFiles/code.dir/Basic/Utils/strlib.cpp.o.d"
+  "CMakeFiles/code.dir/Basic/Utils/tokenScanner.cpp.o"
+  "CMakeFiles/code.dir/Basic/Utils/tokenScanner.cpp.o.d"
+  "CMakeFiles/code.dir/Basic/evalstate.cpp.o"
+  "CMakeFiles/code.dir/Basic/evalstate.cpp.o.d"
+  "CMakeFiles/code.dir/Basic/exp.cpp.o"
+  "CMakeFiles/code.dir/Basic/exp.cpp.o.d"
+  "CMakeFiles/code.dir/Basic/parser.cpp.o"
+  "CMakeFiles/code.dir/Basic/parser.cpp.o.d"
+  "CMakeFiles/code.dir/Basic/program.cpp.o"
+  "CMakeFiles/code.dir/Basic/program.cpp.o.d"
+  "CMakeFiles/code.dir/Basic/statement.cpp.o"
+  "CMakeFiles/code.dir/Basic/statement.cpp.o.d"
+  "code"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
